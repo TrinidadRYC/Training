f7f8bd07ddc7f89c139a814179764f3a5d592b3a
Roberto Gervacio <rgg.correo@gmail.com>
2018-03-15 12:10:32 -0600

    se agrego readme & gitignore

--- Coj/2768-ASimpleTask/aSimpleTask.java
+++ Coj/2768-ASimpleTask/aSimpleTask.java
@@ -1,23 +1,27 @@
+/*
+	28/02/2018
+	Algoritmo: División Modular Binaria
+*/
 import java.util.Scanner;
 public class aSimpleTask {
-  public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
-      long x = scan.nextLong();
-      while(x != 0) {
-        System.out.println(t(x));
-        x = scan.nextLong();
-      }
-  }
+	public static void main(String[] args) {
+			Scanner scan = new Scanner(System.in);
+			long x = scan.nextLong();
+			while(x != 0) {
+				System.out.println(t(x));
+				x = scan.nextLong();
+			}
+	}
 public static long t(long b){
-    b++;
-    long a = 2, mod = 1000000007;
-    long res = 1;
-    while(b>0){
-        if((b&1)==1)
-            res=(a*res)%mod;
-    b>>=1;
-    a=((a)*(a))%mod;
-    }
+		b++;
+		long a = 2, mod = 1000000007;
+		long res = 1;
+		while(b>0){
+				if((b&1)==1)
+						res=(a*res)%mod;
+		b>>=1;
+		a=((a)*(a))%mod;
+		}
 return res-1;
 }
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+#COJ & uVA Problems
+
+Compartimos las soluciones en *Java* para algunos de los problemas de Caribbean Online Judge y University of Valladolid.
+
+Team: **"Trinidad"**
