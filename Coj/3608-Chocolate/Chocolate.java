bd70f82ced5c0d631df2d6757ba90601e2796952
Roberto Gervacio <rgg.correo@gmail.com>
2018-04-25 09:18:10 -0500

    Arreglando chocolate

--- Coj/3608-Chocolate/Chocolate.java
+++ Coj/3608-Chocolate/Chocolate.java
@@ -1,35 +1,29 @@
+/*
+	25/04/2018
+	Algoritmo: ---
+*/
 import java.util.Scanner;
 
 public class Chocolate{
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
-    int casos = input.nextInt();
-    int cont = 0;
-    int turno = 0;
-    int w = 0, h = 0;
-    String hw = "";
-    input.nextLine();
+		int casos = input.nextInt();
+		int cont = 0;
+		int w = 0, h = 0;
+		String hw = "";
+		input.nextLine();
 
-    while (cont<casos){
-      hw = input.nextLine();
-      h = Integer.parseInt(hw.split(" ")[0]);
-      w = Integer.parseInt(hw.split(" ")[1]);
-      while (h!=0){
-        turno++;
-        h--;
-      }
-      while (w!=0){
-        turno++;
-        w--;
-      }
-      if (turno%2==0) {
-        System.out.println(2);
-      }else{
-        System.out.println(1);
-      }
-      turno = 0;
-      cont++;
-    }
-  }
+		while (cont<casos){
+			hw = input.nextLine();
+			//input.nextLine();
+			h = Integer.parseInt(hw.split(" ")[0]);
+			w = Integer.parseInt(hw.split(" ")[1]);
+			if(h == w)
+				System.out.println("2");
+			else
+				System.out.println("1");
+			cont++;
+		}
+	}
 }
