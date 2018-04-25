d3d626628929223f546a1522bae58f8ba6cce4e4
Roberto Gervacio <rgg.correo@gmail.com>
2018-04-25 10:01:38 -0500

    time limit exited 3644

--- Coj/3644-Triangular/Triangular.java
+++ Coj/3644-Triangular/Triangular.java
@@ -1,43 +1,41 @@
 /*
-        18/04/2018
-        Algoritmo:
+	18/04/2018
+	Algoritmo: --
 */
 
 import java.util.Scanner;
 
 public class Triangular{
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    int n = 0;
-    int sum = 0;
-    int triangleCount = 0;
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = 0;
+		int sum = 0;
+		int triangleCount = 0;
+		boolean bandera = false;
 
-    while(input.hasNextLine()){
-      n = input.nextInt();
-      while(n != sum){
-        sum = numTriang(n,sum);
-        triangleCount++;
-        //System.out.println("en while");
-      }
-      System.out.println(triangleCount);
-      sum = 0;
-      triangleCount = 0;
-  }
-  public static int numTriang(int n, int suma){
-    boolean bandera = false;
+		while(input.hasNextLine()){
+			n = input.nextInt();
+			while(n != sum){
+				bandera = false;
+				for (int i=1; i<=n && !bandera; i++)
+				{
+					sum+=i;
+					if (sum==n) {
+						bandera = true;
+					}
+					else if(sum > n){
+						bandera = true;
+						//System.out.println(suma);
+						sum-= i;
+					}
+				}
 
-    for (int i=1; i<=n && !bandera; i++) {
-      suma+=i;
-      if (suma==n) {
-        bandera = true;
-      }
-      else if(suma > n){
-        bandera = true;
-        //System.out.println(suma);
-        suma-= i;
-      }
-    }
-    //System.out.println(suma +" lo que envia");
-    return suma;
-  }
+				triangleCount++;
+				//System.out.println("en while");
+			}
+			System.out.println(triangleCount);
+			sum = 0;
+			triangleCount = 0;
+		}
+	}
 }
